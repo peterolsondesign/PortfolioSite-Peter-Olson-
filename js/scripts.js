49f93fe4ad41b9b23af244c073dd6a9ae8b4da79
peterolsondesign <77470322+peterolsondesign@users.noreply.github.com>
2021-07-20 13:28:51 -0500

    Addresses code errors and accessiblity concerns

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Welcome to my Portfolio! I hope you enjoy your visit! :)")
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle'){
-   x.className += ' responsive';
- }
- else {
-   x.className = 'navtoggle';
- }
+console.log('Welcome to my Portfolio! I hope you enjoy your visit! :)')
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
